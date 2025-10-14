a988491da13c6b8c7c1e189d62589dc2c81db53c
Ivan Ameri <dark.ranked@gmail.com>
2025-10-14 17:19:21 -0300

    Cambio de renderizacion para sidebar en superadmin, falta administrativo y medico

--- front/src/app/dashboard/superadmin/solicitudes/page.tsx
+++ front/src/app/dashboard/superadmin/solicitudes/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import DashboardLayout from "@/components/dashboard/DashboardLayout";
 import SolicitudesMedico from "@/components/dashboard/SolicitudesMedico";
 
 export default function PageSolicitudes() {
-  return (
-    <DashboardLayout>
-      <SolicitudesMedico />
-    </DashboardLayout>
-  );
+  return <SolicitudesMedico />;
 }
